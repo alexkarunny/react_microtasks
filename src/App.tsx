88584c783353de9091fa06ac208c7d94af3cd6e3
alexkarunny <smetan4ik1@gmail.com>
2023-02-28 23:40:55 +0300

    01_nested components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Header} from "./site/Header/Header";
+import {Body} from "./site/Body/Body";
+import {Footer} from "./site/Footer/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Header titleForHeader={'Header'}/>
+            <Body titleForBody={'Content'}/>
+            <Footer titleForFooter={'Bottom'}/>
+        </>
+    );
 }
 
 export default App;
---
+++ src/site/Header/Header.tsx
@@ -0,0 +1,10 @@
+
+type HeaderType = {
+    titleForHeader: string
+}
+
+export const Header = (props: HeaderType) => {
+    return (
+        <div>{props.titleForHeader}</div>
+    )
+}
