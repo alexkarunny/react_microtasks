c346b9d958f5111e48d105892738e9e29e81e51d
alexkarunny <smetan4ik1@gmail.com>
2023-03-01 23:25:13 +0300

    01_filter_start

--- src/site/Body/Body.tsx
+++ src/site/Body/Body.tsx
@@ -22,7 +22,26 @@ const topCars: Array<CarPropsType> = [
     {manufacturer: 'Audi', model: 'rs6'}
 ]
 
+
+type BanknoteType = {
+    banknots: string
+    value: number
+    number: string
+}
+
 export const Body = (props: BodyType) => {
+
+    const [money, setMoney] = useState<BanknoteType[]>([
+        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+    ])
+
 
     const Button1 = (subscriber: string, age: number) => {
         console.log(subscriber)
@@ -59,6 +78,21 @@ export const Body = (props: BodyType) => {
             <Button title={'-'} callback={decreaseA}/>
             <Button title={'0'} callback={resetA}/>
             <h1>{a}</h1>
+
+            <ul>
+                {
+                    money.map((i) => {
+                        return (
+                            <li key={i.number}>
+                                <span>{i.number}</span>
+                                <span>{i.banknots}</span>
+                                <span>{i.value}</span>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+
         </div>
     );
 }
